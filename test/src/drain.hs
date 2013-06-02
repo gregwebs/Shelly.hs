5a1262cd51e6b442c67f92dce44de15119a5fe37
Greg Weber <greg@gregweber.info>
2013-06-01 19:21:19 -0700

    more elaborate drain test

--- test/src/drain.hs
+++ test/src/drain.hs
@@ -1,9 +1,10 @@
 {-# Language OverloadedStrings #-}
 import Shelly
+import Control.Monad (void)
 
 main :: IO ()
-main =
-  shelly $ do
-    res <- run "test/drain.sh" []
+main = void $ shelly $ do
+    res <- cmd "src/drain.sh"
     echo "haskell done"
     echo res
+    cmd "src/printer.sh"
