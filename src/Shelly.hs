745efeca21fef938c1433b3afcea11ce627168f9
Greg Weber <greg@gregweber.info>
2013-12-28 08:38:48 -0800

    remove unused extensions

--- src/Shelly.hs
+++ src/Shelly.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables, DeriveDataTypeable, OverloadedStrings,
-             MultiParamTypeClasses, FlexibleInstances, TypeSynonymInstances,
-             TypeFamilies, IncoherentInstances, GADTs #-}
+             FlexibleInstances,
+             TypeFamilies, ExistentialQuantification #-}
 
 -- | A module for shell-like programming in Haskell.
 -- Shelly's focus is entirely on ease of use for those coming from shell scripting.
