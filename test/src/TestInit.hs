dfbbe694f02d522e7a652c5bf1dfd66eef6f70d8
Greg Weber <greg@gregweber.info>
2014-04-30 13:20:09 -0700

    lifted tests

--- test/src/TestInit.hs
+++ test/src/TestInit.hs
@@ -2,5 +2,9 @@ module TestInit (module Export) where
 
 import Test.HUnit as Export hiding (path) 
 import Test.Hspec as Export
+#ifdef LIFTED
+import Shelly.Lifted as Export
+#else
 import Shelly as Export
+#endif
 import Test.Hspec.HUnit ()
