7afb41cba98d92ce28c2ff9e2ad977e4fbf9c43c
Greg Weber <greg@gregweber.info>
2014-05-27 15:22:38 -0700

    rm_f: don't follow the symlink
    closes #68

--- src/Shelly.hs
+++ src/Shelly.hs
@@ -682,7 +682,7 @@ rm_rf infp = do
 -- Does fail if the file is not a file.
 rm_f :: FilePath -> Sh ()
 rm_f = traceAbsPath ("rm -f " <>) >=> \f ->
-  whenM (test_e f) $ canonic f >>= liftIO . removeFile
+  whenM (test_e f) $ liftIO $ removeFile f
 
 -- | Remove a file.
 -- Does fail if the file does not exist (use 'rm_f' instead) or is not a file.
