gregwebs/Shelly.hs

commit a56ae19959dfbb2ed253c74713ff54aa0a314dc1

inherit stdout when run_ is used
closes #89

This makes things work such as

    ls --color=auto

shelly.cabal
@@ -1,6 +1,6 @@
 Name:       shelly
 
-Version:     1.6.0.1
+Version:     1.6.1
 Synopsis:    shell-like (systems) programming in Haskell
 
 Description: Shelly provides convenient systems programming in Haskell,

src/Shelly.hs
@@ -1062,10 +1062,19 @@ command1_ com args one_arg more_args = run_ com ([one_arg] ++ args ++ more_args)
 -- | the same as 'run', but return @()@ instead of the stdout content
 -- stdout will be read and discarded line-by-line
 run_ :: FilePath -> [Text] -> Sh ()
-run_ = runFoldLines () (\_ _ -> ())
+run_ exe args =
+  -- same a runFoldLines except Inherit Stdout
+  runHandles exe args [OutHandle Inherit] $ \inH _ errH -> do
+    state <- get
+    errVar <- liftIO $ do
+      hClose inH -- setStdin was taken care of before the process even ran
+      (putHandleIntoMVar mempty (|>) errH (sPutStderr state) (sPrintStderr state))
+    errs <- liftIO $ lineSeqToText `fmap` wait errVar
+    modify $ \state' -> state' { sStderr = errs }
+    return ()
 
 liftIO_ :: IO a -> Sh ()
-liftIO_ action = void (liftIO action)
+liftIO_ = void . liftIO
 
 -- | Similar to 'run' but gives the raw stdout handle in a callback.
 -- If you want even more control, use 'runHandles'.

test/shelly-testsuite.cabal
@@ -56,55 +56,32 @@ Executable shelly-testsuite
 -- demonstarated that command output in Shellish was not shown until after the command finished
 -- not necessary anymore
 Executable drain
-  hs-source-dirs: ../src/ examples
+  hs-source-dirs: examples
   main-is: drain.hs
 
-  build-depends:
-    base                      >= 4.6,
-    text                      >= 0.11,
-    async,
-    bytestring                >= 0.10,
-    containers                >= 0.5.0.0,
-    directory                 >= 1.1.0.0 && < 1.3.0.0,
-    process                   >= 1.1.0,
-    unix-compat               < 0.5,
-    system-filepath           >= 0.4.7 && < 0.5,
-    system-fileio             < 0.4,
-    time                      >= 1.3 && < 1.5,
-    mtl                       >= 2,
-    transformers,
-    transformers-base,
-    monad-control,
-    lifted-base,
-    enclosed-exceptions,
-    exceptions                == 0.6.*
+  build-depends: base                      >= 4.6
+               , shelly
+               , text
 
   extensions:
     CPP
 
 Executable run-handles
-  hs-source-dirs: ../src/ examples
+  hs-source-dirs: examples
   main-is: run-handles.hs
 
-  build-depends:
-    base                      >= 4.6,
-    text                      >= 0.11,
-    async,
-    bytestring                >= 0.10,
-    containers                >= 0.5.0.0,
-    directory                 >= 1.1.0.0 && < 1.3.0.0,
-    process                   >= 1.1.0,
-    unix-compat               < 0.5,
-    system-filepath           >= 0.4.7 && < 0.5,
-    system-fileio             < 0.4,
-    time                      >= 1.3 && < 1.5,
-    mtl                       >= 2,
-    transformers,
-    transformers-base,
-    monad-control,
-    lifted-base,
-    enclosed-exceptions,
-    exceptions                == 0.6.*
+  build-depends: base                      >= 4.6
+               , shelly
+               , text
 
   extensions:
     CPP
+
+Executable Color
+  hs-source-dirs: examples
+  main-is: color.hs
+
+  build-depends: base                      >= 4.6
+               , process
+               , shelly
+               , text

commit e8431ced7046845d04234f59e74207f2bb900d97

fix run_ should respect silently
Bug introduced by
a56ae19959dfbb2ed253c74713ff54aa0a314dc1

shelly.cabal
@@ -1,6 +1,6 @@
 Name:       shelly
 
-Version:     1.6.1.1
+Version:     1.6.1.2
 Synopsis:    shell-like (systems) programming in Haskell
 
 Description: Shelly provides convenient systems programming in Haskell,

src/Shelly.hs
@@ -1062,16 +1062,23 @@ command1_ com args one_arg more_args = run_ com (args ++ [one_arg] ++ more_args)
 -- | the same as 'run', but return @()@ instead of the stdout content
 -- stdout will be read and discarded line-by-line
 run_ :: FilePath -> [Text] -> Sh ()
-run_ exe args =
-  -- same a runFoldLines except Inherit Stdout
-  runHandles exe args [OutHandle Inherit] $ \inH _ errH -> do
+run_ exe args = do
     state <- get
-    errVar <- liftIO $ do
-      hClose inH -- setStdin was taken care of before the process even ran
-      (putHandleIntoMVar mempty (|>) errH (sPutStderr state) (sPrintStderr state))
-    errs <- liftIO $ lineSeqToText `fmap` wait errVar
-    modify $ \state' -> state' { sStderr = errs }
-    return ()
+    if sPrintStdout state
+      then runWithColor_
+      else runFoldLines () (\_ _ -> ()) exe args
+  where
+    -- same a runFoldLines except Inherit Stdout
+    -- That allows color to show up
+    runWithColor_ =
+        runHandles exe args [OutHandle Inherit] $ \inH _ errH -> do
+          state <- get
+          errVar <- liftIO $ do
+            hClose inH -- setStdin was taken care of before the process even ran
+            (putHandleIntoMVar mempty (|>) errH (sPutStderr state) (sPrintStderr state))
+          errs <- liftIO $ lineSeqToText `fmap` wait errVar
+          modify $ \state' -> state' { sStderr = errs }
+          return ()
 
 liftIO_ :: IO a -> Sh ()
 liftIO_ = void . liftIO