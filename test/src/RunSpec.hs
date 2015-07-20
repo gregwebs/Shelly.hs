cc8b7da9cfa072e6e17f7cf9845a383229b4fa4e
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2015-07-20 18:42:29 +0200

    Preliminar version of bash and bash_

--- test/src/RunSpec.hs
+++ test/src/RunSpec.hs
@@ -24,3 +24,32 @@ runSpec = do
       res <- shelly $ onCommandHandles (initOutputHandles (flip hSetBinaryMode True))
                     $ run "cat" [ "test/data/nonascii.txt" ]
       res @?= "Selbstverst\228ndlich \252berraschend\n"
+
+  -- Bash-related commands
+  describe "bash" $ do
+    it "simple command" $ do
+      res <- shelly $ bash "echo" [ "wibble" ]
+      res @?= "wibble\n"
+
+    it "without escaping" $ do
+      res <- shelly $ escaping False $ bash "echo" [ "*" ]
+      assert $ "README.md" `elem` T.words res
+
+    it "with binary handle mode" $ do
+      res <- shelly $ onCommandHandles (initOutputHandles (flip hSetBinaryMode True))
+                    $ bash "cat" [ "test/data/nonascii.txt" ]
+      res @?= "Selbstverst\228ndlich \252berraschend\n"
+
+    it "can detect failing commands in pipes" $ do
+      eCode <- shelly $ escaping False $ errExit False $ do
+        bash_ "echo" [ "'foo'", "|", "ls", "\"eoueouoe\"", "|", "echo", "'bar'" ]
+        lastExitCode
+      eCode @?= 1
+
+    it "preserve pipe behaviour" $ do
+      (eCode, res) <- shelly $ escaping False $ errExit False $ do
+        res <- bash "echo" [ "'foo'", "|", "echo", "'bar'" ]
+        eCode <- lastExitCode
+        return (eCode, res)
+      res @?= "bar\n"
+      eCode @?= 0
