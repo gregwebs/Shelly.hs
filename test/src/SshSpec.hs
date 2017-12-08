c6fdf07cd95a615710772a829b0b193e485339c0
Greg Weber <greg@gregweber.info>
2017-12-07 22:59:27 -0800

    escape all ssh arguments

--- test/src/SshSpec.hs
+++ test/src/SshSpec.hs
@@ -5,14 +5,28 @@ import TestInit
 sshSpec :: Spec
 sshSpec = do
   describe "sshCommandText" $ do
-    it "simple command" $ do
-      let res = sshCommandText [("wibble", [])] SeqSsh
-      res @?= "\"wibble\""
+    describe "escaping off" $ do
+        it "simple command" $ do
+          let res = sshCommandText [("wibble", [])] False SeqSsh
+          res @?= "\"wibble\""
 
-    it "space command" $ do
-      let res = sshCommandText [("to", ["outer space"])] SeqSsh
-      res @?= "\"to 'outer space'\""
+        it "space command" $ do
+          let res = sshCommandText [("to", ["outer space"])] False SeqSsh
+          res @?= "\"to 'outer space'\""
 
-    it "multiple space commands" $ do
-      let res = sshCommandText [("to", ["outer space"]), ("and", ["back again"])] SeqSsh
-      res @?= "\"to 'outer space' && and 'back again'\""
+        it "multiple space commands" $ do
+          let res = sshCommandText [("to", ["outer space"]), ("and", ["back again"])] False SeqSsh
+          res @?= "\"to 'outer space' && and 'back again'\""
+
+    describe "escaping on" $ do
+        it "simple command" $ do
+          let res = sshCommandText [("wibble", [])] True SeqSsh
+          res @?= "\"'wibble'\""
+
+        it "space command" $ do
+          let res = sshCommandText [("to", ["outer space"])] True SeqSsh
+          res @?= "\"'to' 'outer space'\""
+
+        it "multiple space commands" $ do
+          let res = sshCommandText [("to", ["outer space"]), ("and", ["back again"])] True SeqSsh
+          res @?= "\"'to' 'outer space' && 'and' 'back again'\""
