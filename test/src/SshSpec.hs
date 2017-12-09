9b7b68472701eddca9b8b5e1be84c9f53c109324
Incomplete <incomplete@aixon.co>
2017-12-09 17:25:34 +0800

    quote SSH remote commands aggressively with single quotes
    This commit introduces the following behaviour:
    
    When a list of remote commands are to be executed via `sshPairs` and
    alike, they will be quoted with single quotes, in a way such that the
    remote commands will see the literal values you passed to it (as
    arguments), this means that no variable expansion and alike will done
    on either the local shell or the remote shell, and that if there are
    single or double quotes in your arguments, they need not to be quoted
    manually.

--- test/src/SshSpec.hs
+++ test/src/SshSpec.hs
@@ -1,18 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
 module SshSpec ( sshSpec ) where
 
 import TestInit
+import qualified Data.Text as T
 
 sshSpec :: Spec
 sshSpec = do
+  let q = "'" -- a single quote
+  let qq = "'\\''" -- quote of a single quote
+  let qqq = T.concat [qq, "\\", qq, qq] -- quote of qq
   describe "sshCommandText" $ do
     it "simple command" $ do
       let res = sshCommandText [("wibble", [])] SeqSsh
-      res @?= "\"wibble\""
+      res @?= T.concat [q, qq, "wibble", qq, q]
 
     it "space command" $ do
       let res = sshCommandText [("to", ["outer space"])] SeqSsh
-      res @?= "\"to 'outer space'\""
+      res @?= T.concat [q, qq, "to", qq, " ", qq, "outer space", qq ,q]
 
     it "multiple space commands" $ do
       let res = sshCommandText [("to", ["outer space"]), ("and", ["back again"])] SeqSsh
-      res @?= "\"to 'outer space' && and 'back again'\""
+      res @?= T.concat
+                [ q, qq, "to", qq, " ", qq, "outer space", qq
+                , " && "
+                , qq, "and", qq, " ", qq, "back again", qq, q
+                ]
+
+    it "commands with quotes and spaces" $ do
+      let res = sshCommandText [ ("echo", ["Godfater's brother, Tom says: \"huh??\""])
+                               , ("foo", ["--dir", "Tom's father/"])] SeqSsh
+      res @?= T.concat
+                [ q, qq, "echo", qq, " "
+                , qq, "Godfater", qqq, "s brother, Tom says: \"huh??\"", qq
+                , " && "
+                , qq, "foo", qq, " "
+                , qq, "--dir", qq, " "
+                , qq, "Tom", qqq, "s father/", qq, q
+                ]
